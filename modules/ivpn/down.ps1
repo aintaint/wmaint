e2b9b159a3087cd6b6325ad65d838d129d10fca9
aintaint <78907061+aintaint@users.noreply.github.com>
2024-03-06 07:57:00 -0800

    rm ivpn

--- modules/ivpn/down.ps1
+++
@@ -1,5 +0,0 @@
-ivpn disconnect
-ivpn firewall -persistent_off
-ivpn firewall -off
-
-..\system\disconnectwifi.ps1
--- modules/ivpn/up.ps1
+++
@@ -1,9 +0,0 @@
-..\edge\killall.ps1
-..\system\adddoh.ps1
-
-ivpn connect -exit_svr ch.wg.ivpn.net -antitracker -p wg -l hu.wg.ivpn.net
-
-Read-Host -Prompt "Press any key to continue"
-
-ivpn firewall -persistent_on
-ivpn firewall -on
