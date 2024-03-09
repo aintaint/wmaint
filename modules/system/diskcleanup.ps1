753e46a3b8268a80da0036d44761b60958543499
aintaint <78907061+aintaint@users.noreply.github.com>
2024-03-09 03:49:14 -0800

    move net

--- modules/system/diskcleanup.ps1
+++ modules/system/diskcleanup.ps1
@@ -1,4 +1,7 @@
-cleanmgr /verylowdisk /autoclean
+Start-Process -FilePath cleanmgr.exe -ArgumentList '/VERYLOWDISK /AUTOCLEAN'
 
+While ((Get-Process -Name cleanmgr).MainWindowTitle -ne "Disk Space Notification") {Start-Sleep 1}
+
+Stop-Process -Name cleanmgr -Force -ErrorAction SilentlyContinue
 
 # sageset, tuneup reg keys https://learn.microsoft.com/en-us/troubleshoot/windows-server/backup-and-storage/automating-disk-cleanup-tool#registry-key-information
